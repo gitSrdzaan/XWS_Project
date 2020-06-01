0b158eb5d84ec0a1d6ab8bc45918696ec24c8696
gitSrdzaan <srdjan_kralj_97@live.com>
2020-06-01 20:45:15 +0200

    hello world kontroleri

---
+++ Microservices/advertservice/src/main/java/xws/microservis/advertservice/controller/HelloWorld.java
@@ -0,0 +1,19 @@
+package xws.microservis.advertservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/a")
+public class HelloWorld {
+
+	@GetMapping
+	public ResponseEntity<String> pozdrav(){
+        String ime = "Hello World advert";
+        return new ResponseEntity<>(ime, HttpStatus.OK) ;
+    }
+	
+}
