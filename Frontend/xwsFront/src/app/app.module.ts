c921ba112e3a8e8d5f7fc31d7d8f23c2f2acb862
Artukov <nikolartukov@gmail.com>
2020-07-07 17:40:07 +0200

    Fronta malo

---
+++ Frontend/xwsFront/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderCompComponent } from './header-comp/header-comp.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { LoginCompComponent } from './login-comp/login-comp.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderCompComponent, LoginCompComponent],
+  imports: [
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+//export class AppBootstrapModule {}
+export class AppModule {}
