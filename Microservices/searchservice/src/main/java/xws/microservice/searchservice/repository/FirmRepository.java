5c5223dde8de6bdc5205d6a51bb83482750c5efa
gitSrdzaan <srdjan_kralj_97@live.com>
2020-07-08 21:34:18 +0200

    osnnovna pretraga automobila

--- Microservices/searchservice/src/main/java/xws/microservice/searchservice/repository/FirmRepository.java
+++ Microservices/searchservice/src/main/java/xws/microservice/searchservice/repository/FirmRepository.java
@@ -1,11 +1,17 @@
 package xws.microservice.searchservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import xws.microservice.searchservice.model.Firm;
 
+import java.util.List;
+
 @Repository
 public interface FirmRepository extends JpaRepository<Firm, Long> {
 
+    @Query(value ="SELECT * FROM firm f WHERE f.country = :country and f.city = :city ",nativeQuery = true)
+    List<Firm> findAllByPlace(@Param("country") String country, @Param("city") String city);
 }
