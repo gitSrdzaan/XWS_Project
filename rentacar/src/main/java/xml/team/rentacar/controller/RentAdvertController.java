60b8a7e4bc76f1ddb4387822f933b875cf18265c
gitSrdzaan <srdjan_kralj_97@live.com>
2020-08-31 13:40:08 +0200

    CrossOrigin na RentAdvertController

--- rentacar/src/main/java/xml/team/rentacar/controller/RentAdvertController.java
+++ rentacar/src/main/java/xml/team/rentacar/controller/RentAdvertController.java
@@ -23,6 +23,7 @@ import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/advert")
+@CrossOrigin(origins = "*")
 public class RentAdvertController {
 
 	@Autowired
@@ -53,7 +54,7 @@ public class RentAdvertController {
 	}
 	
 	@PostMapping(value = "/new",consumes = "application/json", produces ="application/json")
-	public ResponseEntity<?> aoddNewRentAdvert(@RequestBody RentAdvertDTO raDTO){
+	public ResponseEntity<?> addNewRentAdvert(@RequestBody RentAdvertDTO raDTO){
 		
 		Car car = carService.findCar(raDTO.getCarID());
 		if(car == null) {
