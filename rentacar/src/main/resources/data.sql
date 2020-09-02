0e60baa9242fbf5434724d849c4556c9c3879608
markaya <markoristic15@gmail.com>
2020-09-02 19:24:54 +0200

    Fix errors

--- rentacar/src/main/resources/data.sql
+++ rentacar/src/main/resources/data.sql
@@ -3,8 +3,6 @@ insert into car_mark values(1,'BMW');
 insert into car_model values(1,'X5',1);
 insert into car_class values(1,'SUV');
 insert into model_class values(1,1);
-insert into car_fuel values(1,'dizel');
-insert into transmission values(1,'manuelni');
 insert into firm values(123 , 'mifa' , 'fafsa' , 'ffsafa' , 'fasfa' , 'ffasfsa');
 
 
@@ -21,7 +19,7 @@ transmission) values (123,'SUV','dizel', 2,'BMW',0.0,'X5','registration',0,500,'
 insert into price_list(id,price_per_day, price_per_kilometer, pricecdw,firm_id)
 values(123,2,4,60,123);
 
-insert into rent_advert(id ,advert_start_date,advert_end_date, car_id , firm , price_list_id)
+insert into rent_advert(id ,advert_start_date,advert_end_date, car_id , firm , pricelist_id)
 values(123,parsedatetime('05-07-2020 12:00:00.001', 'dd-MM-yyyy hh:mm:ss.SS'),parsedatetime('15-07-2020 12:00:00.001', 'dd-MM-yyyy hh:mm:ss.SS'),
 123,123,123);
 
