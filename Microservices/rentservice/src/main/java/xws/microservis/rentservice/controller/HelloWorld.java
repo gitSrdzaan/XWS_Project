532ed5188a55653a144381f29011c81b636782c4
markaya <markoristic15@gmail.com>
2020-09-03 14:31:08 +0200

    duracija reda

--- Microservices/rentservice/src/main/java/xws/microservis/rentservice/controller/HelloWorld.java
+++ Microservices/rentservice/src/main/java/xws/microservis/rentservice/controller/HelloWorld.java
@@ -4,14 +4,13 @@ package xws.microservis.rentservice.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import xws.microservis.rentservice.soap.SoapClient;
+
 
 @RestController
 @RequestMapping(value = "/a")
 public class HelloWorld {
 
 
-    private SoapClient soapClient;
 
 	@GetMapping
 	public ResponseEntity<String> pozdrav(){
@@ -19,14 +18,6 @@ public class HelloWorld {
         return new ResponseEntity<>(ime, HttpStatus.OK) ;
     }
 
-    @GetMapping(path = "/soap/{id}")
-    public ResponseEntity<String> testSoap(@PathVariable Long id){
 
-	    //String carId = soapClient.getRentAdvert(id).getRentAdvert().getOwner();
-
-	   // return new ResponseEntity<>(carId , HttpStatus.OK);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
 	
 }
